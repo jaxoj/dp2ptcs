49321ac1ffdfeb444f6acd4034b3950e3d4ca0f3
monsef <jaxo.j@outlook.com>
2026-03-13 06:24:28 +0100

    injecting the static discoverer in the main function

--- cmd/node/main.go
+++ cmd/node/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"dp2ptcs/internal/crypto"
+	"dp2ptcs/internal/dht"
+	"dp2ptcs/internal/network"
 	"dp2ptcs/internal/usecase"
 	"encoding/hex"
 	"fmt"
@@ -16,20 +19,28 @@ func main() {
 	keyPath := "node.key"
 
 	// Execute the application injecting standard output for log
-	if err := run(os.Stdout, keyPath); err != nil {
+	if err := run(os.Stdout, keyPath, os.Args[1:]); err != nil {
 		log.Fatalf("[Fetal]: %v", err)
 	}
 }
 
 // run separates dependency injection and execution from the main() scope,
 // allowing us to test the application wiring cleanly.
-func run(out io.Writer, keyPath string) error {
+func run(out io.Writer, keyPath string, args []string) error {
 	// Frameworks & Drivers: Instantiate concrete dependencies
 	entropy := rand.Reader
 	store := crypto.NewFileIdentityStore(keyPath)
 
+	// Define our bootstrap node (Command Post)
+	cpID := bytes.Repeat([]byte{0xAA}, 32)
+	cpPeer, _ := dht.NewPeer(cpID, []string{"10.55.0.1:9000"})
+
+	// Instantiate the interface adapter for discovery
+	discoverer := network.NewStaticDiscoverer([]*dht.Peer{cpPeer})
+
 	// Interface Adapters ->  Wire the manager
 	manager := usecase.NewIdentityManager(store, entropy)
+	connectionManager := usecase.NewConnectionManager(discoverer)
 
 	// Execute Core Logic
 	identity, err := manager.LoadOrCreate()
@@ -41,6 +52,23 @@ func run(out io.Writer, keyPath string) error {
 	fmt.Fprintln(out, "Tactical Node Initialized.")
 	// The Node ID is binary, so we encode it to a readable hex string
 	fmt.Fprintf(out, "Node ID: %s\n", hex.EncodeToString(identity.NodeID))
+
+	// If target ID was provided via CLI, attempt discovery
+	if len(args) > 0 {
+		targetHex := args[0]
+		targetID, err := hex.DecodeString(targetHex)
+		if err != nil {
+			return fmt.Errorf("invalid hex encoding for target ID: %w", err)
+		}
+
+		fmt.Fprintf(out, "Attempting to resolve peer: %s...\n", targetHex)
+
+		peer, err := connectionManager.ResolvePeer(targetID)
+		if err != nil {
+			return fmt.Errorf("Peer discovery failed: %w", err)
+		}
+		fmt.Fprintf(out, "Successfully resolved peer! Available at addresses: %v\n", peer.Addresses)
+	}
 
 	return nil
 }
